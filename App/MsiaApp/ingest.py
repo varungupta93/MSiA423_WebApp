75b9294eee31590c36afc355a9ed989f78686a5b
Varun Gupta <varungupta2018@u.northwestern.edu>
2018-03-18 15:06:49 -0500

    Updated with corrections to forecast pull, and RDS

---
+++ App/MsiaApp/ingest.py
@@ -0,0 +1,31 @@
+from MsiaApp import db
+from MsiaApp.models import Weather
+import datetime
+
+"""Ingestion module
+
+This module is meant to be called to ingest data and place it into db. Once an API is used
+the module can be extended with a function that calls the API and puts the data into the db.
+
+Functionality presumes that the db has already been created. 
+
+"""
+
+def seed_db():
+    """Seed a preexisting db with dat
+
+    Returns:
+
+    """
+    db.session.query(weather).delete()
+    weatherForecast = GetForecast()
+    forecast1 = Weather(Date=weatherForecast.iloc[0]['Date'].strftime("%m/%d/%Y"),
+    					MeanAppTemp = weatherForecast.iloc[0]["MeanAppTemp"],
+    					PrecipProb = weatherForecast.iloc[0]["PrecipProb"],
+    					PrecipIntensity = weatherForecast.iloc[0]["PrecipIntensity"] )
+    db.session.add(forecast1)
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    seed_db()
---
+++ App/create_db.py
@@ -0,0 +1,9 @@
+from MsiaApp import db
+import app.data_model
+
+def create_db():
+    db.create_all()
+
+if __name__ == '__main__':
+    create_db()
+    print('Database created.')
